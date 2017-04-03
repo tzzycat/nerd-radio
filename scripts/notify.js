5f6970387516ad59fd9a680cbe5e0e129d8dd3f3
tzzycat <tzzycat@gmail.com>
2017-04-03 17:01:06 +0800

    Update notify.js
    To make it pop up at most one notification

--- scripts/notify.js
+++ scripts/notify.js
@@ -2,16 +2,22 @@
  * Dependencies: jQuery, corresponding CSS file (notify.css)
  */
 let notifyContainer = document.getElementById('notify-container'),
-    notifyHideDelay = 5;  // seconds
+    notifyHideDelay = 5,  // seconds
+    notifyAppend = false;
 
 function notify(content) {
   let notifyElement = document.createElement('div');
   notifyElement.className = 'notify-entry';
   notifyElement.innerHTML = content;
 
+  if (!notifyAppend) {
+    while (notifyContainer.lastChild)
+      notifyContainer.removeChild(notifyContainer.lastChild);
+  }
   notifyContainer.appendChild(notifyElement);
 
   $(notifyElement).delay(notifyHideDelay * 1000).fadeOut(function() {
-    this.parentNode.removeChild(this);
+    if (this.parentNode)
+      this.parentNode.removeChild(this);
   });
 }
